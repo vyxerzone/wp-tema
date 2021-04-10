f38abe9748d8ef0c0cbc15bb81432b731f556111
InterZone Web <interzonegdl@gmail.com>
2021-04-10 05:29:25 -0500

    Modif. index a php, insertando CSS

--- wp-content/themes/iztheme/functions.php
+++ wp-content/themes/iztheme/functions.php
@@ -12,10 +12,17 @@ add_action( 'wp_enqueue_scripts', 'bootstrap_css');
 
 // Incluir Bootstrap JS
 function bootstrap_js() {
-	wp_enqueue_script( 'bootstrap_js', 
-  					get_stylesheet_directory_uri() . '/js/bootstrap.min.js', 
+	wp_enqueue_script( 'popper', 
+  					get_stylesheet_directory_uri() . 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.1/umd/popper.min.js', 
   					array('jquery'), 
+  					'2.9.1', 
+  					true); 
+
+	wp_enqueue_script( 'bootstrap_js', 
+  					get_stylesheet_directory_uri() . 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js', 
+  					array('popper'), 
   					'5.0', 
   					true); 
+                      
 }
 add_action( 'wp_enqueue_scripts', 'bootstrap_js');
