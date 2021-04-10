bef2e0f33ef8f368572cb5d84a327f378b55dfaa
InterZone Web <interzonegdl@gmail.com>
2021-04-10 05:43:26 -0500

    Modif. index a php, insertando CSS

---
+++ wp-content/themes/iztheme/footer.php
@@ -0,0 +1,12 @@
+<footer class="container-fluid py-4 text-center text-white bg-primary">
+    <h5>TusCursos.uno!</h5>
+  </footer>
+  <!--contenido-->
+  <!-- Optional JavaScript; choose one of the two! -->
+
+    
+
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- wp-content/themes/iztheme/index.php
+++ wp-content/themes/iztheme/index.php
@@ -83,16 +83,4 @@
     
 </div>
   </div>
-
-  <footer class="container-fluid py-4 text-center text-white bg-primary">
-    <h5>TusCursos.uno!</h5>
-  </footer>
-  <!--contenido-->
-  <!-- Optional JavaScript; choose one of the two! -->
-
-    
-
-
-    <?php wp_footer(); ?>
-  </body>
-</html>
+  <?php get_footer(); ?>
