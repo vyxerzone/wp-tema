be568f4ab28f28b381ba80649b6b8c0cc2b81510
InterZone Web <interzonegdl@gmail.com>
2021-04-10 05:40:58 -0500

    Modif. index a php, insertando CSS

---
+++ wp-content/themes/iztheme/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <title>Hello, world!</title>
+      <?php wp_head(); ?>
+  </head>
+  <body>
+
+    <!--menu navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">TusCursos.uno!</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav ml-auto">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </div>
+          </div>
+        </div>
+      </nav>
+
+<!--menu navbar--> 
--- wp-content/themes/iztheme/index.php
+++ wp-content/themes/iztheme/index.php
@@ -1,34 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      <title>Hello, world!</title>
-      <?php wp_head(); ?>
-  </head>
-  <body>
-
-    <!--menu navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">TusCursos.uno!</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <div class="navbar-nav ml-auto">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </div>
-          </div>
-        </div>
-      </nav>
-
-<!--menu navbar--> 
+<?php get_header(); ?>
 <!--Slider-->
 <div id="carouselExampleFade" class="carousel slide carousel-fade mt-5" data-bs-ride="carousel">
     <div class="carousel-inner">
@@ -121,7 +91,7 @@
   <!-- Optional JavaScript; choose one of the two! -->
 
     
-  
+
 
     <?php wp_footer(); ?>
   </body>
